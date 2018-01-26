88448c3151fad50d9fc842c39239b376b8ba1f70
ManuGamer96_ <manuelmaterazzo@libero.it>
2018-01-26 14:32:01 +0100

    boh

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ GROUP_ID = "-185377680"
 #Declare the Forum
 FORUM_NAME = "SteamGamersInside"
 FORUM_PROTOCOL = "http://"
-FORUM_URL = "lorem-rss.herokuapp.com"
+FORUM_URL = "createfeed.wprssaggregator.com"
 
 # Declare the Feeds
-rss_main_feed = (FORUM_PROTOCOL + FORUM_URL + '/feed?unit=second&interval=300')
+rss_main_feed = (FORUM_PROTOCOL + FORUM_URL + '/extract.php?url=https%3A%2F%2Fwww.reddit.com%2Fr%2FFreeGamesOnSteam%2F&in_id_or_class=title+may-blank+outbound')
